797b70b27cc8f79b93562c61493a3a91159a07c4
zerogpm <zerogpm@gmail.com>
2024-01-05 22:28:21 -0800

    test

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("your code here what!");
+function add(num1, num2) {
+    return num1 + num2;
+}
+var num1 = 4;
+var num2 = 5;
+console.log(add(num1, num2));
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("your code here what!");
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const num1 = 4;
+const num2 = 5;
+
+console.log(add(num1, num2));
